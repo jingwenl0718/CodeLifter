026eb084032304adc02f8b879064a09804dc2e95
Jingwen Li <lijingwenot@gmail.com>
2022-10-12 23:22:00 -0700

    First commit

---
+++ src/main/java/com/jingwenli/codelifter/CodelifterJavaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.jingwenli.codelifter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodelifterJavaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodelifterJavaprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jingwenli/codelifter/CodelifterJavaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jingwenli.codelifter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodelifterJavaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
